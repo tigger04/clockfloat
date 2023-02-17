62fcbf22d77024c1419acc4d48cffed238101ae8
Tadhg O'Brien <tigger04@gmail.com>
2023-02-17 23:46:13 +0000

    Initial Commit

---
+++ clockfloat/clockfloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockfloatApp.swift
+//  clockfloat
+//
+//  Created by tigger on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct clockfloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
