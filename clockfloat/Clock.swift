6d7a17aea1f68ab6b715750150449566f0bdfca3
Tadhg O'Brien <tigger04@gmail.com>
2023-03-06 05:38:45 +0000

    surewhynot

--- clockfloat/Clock.swift
+++ clockfloat/Clock.swift
@@ -23,99 +23,87 @@
 import Cocoa
 
 class Clock: NSObject, NSApplicationDelegate {
-    var dater : EvasiveWindow?
-    var timer : EvasiveWindow?
+   var dater: EvasiveWindow?
+   var timer: EvasiveWindow?
 //    var screenW : CGFloat = NSScreen.main!.frame.width
 //    var screenH : CGFloat = NSScreen.main!.frame.height
-    
-    var dateFont : String = "New"
-    var dateFontSize : CGFloat = 14
-    
-    var timeFont : String = "New"
-    var timeFontSize : CGFloat = 22
-    
+
+   var dateFont: String = "New"
+   var dateFontSize: CGFloat = 14
+
+   var timeFont: String = "New"
+   var timeFontSize: CGFloat = 22
+
 //    var xpadding : CGFloat = 10
 //    var ypadding : CGFloat = 10
 //    var wMarginRatio : CGFloat = 1.1
 //    var hMarginRatio : CGFloat = 1.3
-    
+
 //    var clockRect : CGRect = CGRect(x:0, y:0, width: 0, height: 0)
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-        self.initTimer()
-        self.initDater()
-    }
 
-    func initLabel(font: NSFont, format: String, interval: TimeInterval, dummytext: String) -> NSTextField {
-        let formatter = DateFormatter()
-        formatter.dateFormat = format
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      self.initTimer()
+      self.initDater()
+   }
 
-        let label = NSTextField()
-        label.font = font
-        label.isBezeled = false
-        label.isEditable = false
-        label.drawsBackground = false
-        label.alignment = .center
-        label.stringValue = dummytext
-        
+   func initLabel(font: NSFont, format: String, interval: TimeInterval, dummytext: String) -> NSTextField {
+      let formatter = DateFormatter()
+      formatter.dateFormat = format
+
+      let label = NSTextField()
+      label.font = font
+      label.isBezeled = false
+      label.isEditable = false
+      label.drawsBackground = false
+      label.alignment = .center
+      label.stringValue = dummytext
+
 //        label.textColor = NSColor(red: 1, green: 1, blue: 1, alpha: 1-(1/3)*(1/3))
-        label.textColor = NSColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+      label.textColor = NSColor(red: 1, green: 1, blue: 1, alpha: 0.5)
 //        label.sizeToFit()
 
-        let timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
-            label.stringValue = formatter.string(from: Date())
-        }
-        timer.tolerance = interval / 10
-        timer.fire()
+      let timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+         label.stringValue = formatter.string(from: Date())
+      }
+      timer.tolerance = interval / 10
+      timer.fire()
 
-        return label
-    }
-    
-    func initWindow(label: NSTextField, stickWin: EvasiveWindow, name: String) -> EvasiveWindow {
-        let window = self.initWindow(label: label, name: name)
-        window.setStickToWindow(win: stickWin)
-        
-        return window
-    }
-    
-    func initWindow(label: NSTextField, name: String) -> EvasiveWindow {
+      return label
+   }
 
-        let window = EvasiveWindow(label: label, name: name)
-        
-        return window
-    }
+   func initWindow(label: NSTextField, name: String, stickWin: EvasiveWindow? = nil) -> EvasiveWindow {
+      let window = EvasiveWindow(label: label, name: name, stickWin: stickWin)
 
-    func initDater() {
-        let label = self.initLabel(
-            font      : NSFont(name: dateFont, size: dateFontSize)!,
-            format    : "E d",
-            interval  : 10,
-            dummytext : "XXX XX"
-        )
-        
-        self.dater = self.initWindow(
-            label    : label,
-            stickWin : self.timer!,
-            name     : "dater"
-        )
-    }
+      return windowa
+   }
 
-    func initTimer() {
-        
-        let label = self.initLabel(
-            //            font     : NSFont.monospacedDigitSystemFont(ofSize: 36, weight: .regular),
-            font     : NSFont(name: timeFont, size: timeFontSize)!,
-            format   : "HH:mm",
-            interval : 1,
-            dummytext: "99:99"
+   func initDater() {
+      let label = self.initLabel(
+         font: NSFont(name: self.dateFont, size: self.dateFontSize)!,
+         format: "E d",
+         interval: 10,
+         dummytext: "XXX XX"
+      )
 
-        )
-        
-        self.timer = self.initWindow(
-            label    : label,
-            name     : "timer"
-        )
-    }
+      self.dater = self.initWindow(
+         label: label,
+         name: "dater",
+         stickWin: self.timer!
+      )
+   }
+
+   func initTimer() {
+      let label = self.initLabel(
+         //            font     : NSFont.monospacedDigitSystemFont(ofSize: 36, weight: .regular),
+         font: NSFont(name: self.timeFont, size: self.timeFontSize)!,
+         format: "HH:mm",
+         interval: 1,
+         dummytext: "99:99"
+      )
+
+      self.timer = self.initWindow(
+         label: label,
+         name: "timer"
+      )
+   }
 }
-
--- clockfloat/EvasiveWindow.swift
+++ clockfloat/EvasiveWindow.swift
@@ -23,168 +23,169 @@
 import Cocoa
 
 class EvasiveWindow: NSWindow {
-    
-    var screenW : CGFloat = NSScreen.main!.frame.width
-    var screenH : CGFloat = NSScreen.main!.frame.height
-    
-    var dateFont : String = "New"
-    var dateFontSize : CGFloat = 14
-    
-    var timeFont : String = "New"
-    var timeFontSize : CGFloat = 22
-    
-    var xpadding : CGFloat = 10
-    var ypadding : CGFloat = 10
-    var wMarginRatio : CGFloat = 3.7
-    var hMarginRatio : CGFloat = 3.7
-    
-    var stickToWindow : EvasiveWindow? = nil
-    var orientation : Int = 0
-    
-    var name : String = "untitled"
-    
-    //    override public init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool)
-    public init(label: NSTextField, name: String) {
-        
-        self.name = name
-        
-        //        super.init( contentRect: NSMakeRect(300,300,300,300),
-        //                    styleMask:   .borderless,
-        //                    backing:     .buffered,
-        //                    defer:       true)
-        //
-        let winWidth = label.fittingSize.width * wMarginRatio
-        let winHeight = label.fittingSize.height * hMarginRatio
-        
-        let winRect = NSRect(x:0, y:0,
-                             width: winWidth,
-                             height: winHeight)
-        
-        
-        super.init( contentRect: winRect,
-                    styleMask:   .borderless,
-                    backing:     .buffered,
-                    defer:       true)
-         
-        print("\(self.name).init winWidth=\(winWidth), winHeight=\(winHeight)")
-        print("\(self.name).init frame.width=\(self.frame.width), frame.height=\(self.frame.height)")
-       
-        // hack to get the damned thing vertically centered
-        // thanks for nothing Cocoa
-        let stringHeight: CGFloat = label.fittingSize.height
-        let cell = NSTableCellView()
-        cell.frame = NSRect(x: 0, y: 0, width: winWidth, height: label.fittingSize.height)
-        label.frame = cell.frame
-        label.alignment = .center
-        
-        let frame = label.frame
-        var titleRect:  NSRect = label.cell!.titleRect(forBounds: frame)
-        
-        //        titleRect.size.height = label.fittingSize.height
-        //        titleRect.size.width = label.fittingSize.width
-        titleRect.origin.y = frame.origin.y + ( winHeight - stringHeight ) / 2
-        label.frame = titleRect
-        cell.addSubview(label)
-        
-        self.contentView = cell
-        self.ignoresMouseEvents = false
-        self.isMovableByWindowBackground = true
-        self.level = .floating
-        self.collectionBehavior = .canJoinAllSpaces
-        self.backgroundColor = NSColor(red: 0, green: 0, blue: 0, alpha: 0.25)
-        self.orderFrontRegardless()
-        self.refreshOrigin()
-    }
-    
-    func move() {
-        print("\(self.name) move")
-        self.orientation = Int( self.orientation + 1 ) % 4
-        self.refreshOrigin()
-    }
-    
-    func refreshOrigin() {
-        print("\(self.name) refresh origin. I have an orientation of \(self.orientation)")
-        
-        if let stickWin = self.stickToWindow {
-            print("\(self.name) must stick to \(stickWin.name)")
-            
-            if self.orientation < 2 {
-                let x = stickWin.frame.origin.x
-                let y = stickWin.frame.origin.y - stickWin.frame.height
-                self.setFrameOrigin(NSPoint(x:x, y:y))
-            }
-            else
-            {
-                let x = stickWin.frame.origin.x
-                let y = stickWin.frame.origin.y + stickWin.frame.height
-                self.setFrameOrigin(NSPoint(x:x, y:y))
-            }
-        }
-        else {
-            
-            print("\(self.name) is free and easy")
-            
-            let screenW = self.screen?.frame.width ?? 0
-            let screenH = self.screen?.frame.height ?? 0
-            //        let screenW = NSScreen.main!.frame.width
-            //        let screenH = NSScreen.main!.frame.height
-            let width = self.frame.width
-            let height = self.frame.height
-            
-            var x : CGFloat
-            var y : CGFloat
-            
-            switch self.orientation {
-            case 0: // topleft
-                x = xpadding
-                y = screenH - height - ypadding
-            case 1: // topright
-                x = screenW - width - xpadding
-                y = screenH - height - ypadding
-            case 2: // bottomright
-                x = screenW - width - xpadding
-                y = ypadding
-            case 3: // bottomleft
-                x = xpadding
-                y = ypadding
-            default:
-                exit(1)
-            }
-            
-            self.setFrameOrigin(NSPoint(x:x, y:y))
-        }
-        //        self.setContentSize(NSSize(width: width, height: height))
-    }
-    
-    public func getOrientation() -> Int {
-        return self.orientation
-    }
-    
-    public func setStickToWindow(win: EvasiveWindow) {
-        self.stickToWindow = win
-        self.refreshOrigin()
-    }
-    
-    override func mouseEntered(with event: NSEvent) {
-        super.mouseEntered(with: event)
-        print("mouse entered")
-        self.move()
-    }
-    
-    override func mouseExited(with event: NSEvent) {
-        super.mouseExited(with: event)
-        print("mouse exited")
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        super.mouseDown(with: event)
-        print("mouse down")
-        //        self.move()
-    }
-    
-    override func rightMouseDown(with event: NSEvent) {
-        super.rightMouseDown(with: event)
-        print("right mouse button down")
-        self.move()
-    }
+   var screenW: CGFloat = NSScreen.main!.frame.width
+   var screenH: CGFloat = NSScreen.main!.frame.height
+
+   var dateFont: String = "New"
+   var dateFontSize: CGFloat = 14
+
+   var timeFont: String = "New"
+   var timeFontSize: CGFloat = 22
+
+   var xpadding: CGFloat = 10
+   var ypadding: CGFloat = 10
+   var wMarginRatio: CGFloat = 3.7
+   var hMarginRatio: CGFloat = 3.7
+
+   var stickToWindow: EvasiveWindow?
+   var orientation: Int = 0
+
+   var name: String = "untitled"
+
+   public init(label: NSTextField, name: String,
+               stickWin: EvasiveWindow? = nil)
+   {
+      self.name = name
+
+      let winHeight = label.fittingSize.height * self.hMarginRatio
+      var winWidth = label.fittingSize.width * self.wMarginRatio
+
+      if stickWin != nil {
+         self.stickToWindow = stickWin
+         winWidth = self.stickToWindow!.frame.width
+      }
+
+      let winRect = NSRect(x: 0, y: 0,
+                           width: winWidth,
+                           height: winHeight)
+
+      super.init(contentRect: winRect,
+                 styleMask: .borderless,
+                 backing: .buffered,
+                 defer: true)
+
+      print("\(self.name).init winWidth=\(winWidth), winHeight=\(winHeight)")
+      print("\(self.name).init frame.width=\(self.frame.width), frame.height=\(self.frame.height)")
+
+      // hack to get the damned thing vertically centered
+      // thanks for nothing Cocoa
+      let stringHeight: CGFloat = label.fittingSize.height
+      let cell = NSTableCellView()
+      cell.frame = NSRect(x: 0, y: 0, width: winWidth, height: label.fittingSize.height)
+      label.frame = cell.frame
+      label.alignment = .center
+
+      let frame = label.frame
+      var titleRect: NSRect = label.cell!.titleRect(forBounds: frame)
+
+      //        titleRect.size.height = label.fittingSize.height
+      //        titleRect.size.width = label.fittingSize.width
+      titleRect.origin.y = frame.origin.y + (winHeight - stringHeight) / 2
+      label.frame = titleRect
+      cell.addSubview(label)
+
+      self.contentView = cell
+      self.ignoresMouseEvents = false
+      self.isMovableByWindowBackground = true
+      self.level = .floating
+      self.collectionBehavior = .canJoinAllSpaces
+      self.backgroundColor = NSColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+      self.orderFrontRegardless()
+      self.refreshOrigin()
+   }
+
+   func move() {
+      print("\(self.name) move")
+      self.orientation = Int(self.orientation + 1) % 4
+      self.refreshOrigin()
+   }
+
+   //    func getWidth() -> CGFloat {
+   //        return self.frame.width
+   //    }
+
+   func refreshOrigin() {
+      print("\(self.name) refresh origin. I have an orientation of \(self.orientation)")
+
+      if let stickWin = self.stickToWindow {
+         print("\(self.name) must stick to \(stickWin.name)")
+
+         if self.orientation < 2 {
+            let x = stickWin.frame.origin.x
+            let y = stickWin.frame.origin.y - self.frame.height
+            self.setFrameOrigin(NSPoint(x: x, y: y))
+         }
+         else {
+            let x = stickWin.frame.origin.x
+            let y = stickWin.frame.origin.y + self.frame.height
+            self.setFrameOrigin(NSPoint(x: x, y: y))
+         }
+      }
+      else {
+         print("\(self.name) is free and easy")
+
+         let screenW = self.screen?.frame.width ?? 0
+         let screenH = self.screen?.frame.height ?? 0
+         //        let screenW = NSScreen.main!.frame.width
+         //        let screenH = NSScreen.main!.frame.height
+         let width = self.frame.width
+         let height = self.frame.height
+
+         var x: CGFloat
+         var y: CGFloat
+
+         switch self.orientation {
+         case 0: // topleft
+            x = self.xpadding
+            y = screenH - height - self.ypadding
+         case 1: // topright
+            x = screenW - width - self.xpadding
+            y = screenH - height - self.ypadding
+         case 2: // bottomright
+            x = screenW - width - self.xpadding
+            y = self.ypadding
+         case 3: // bottomleft
+            x = self.xpadding
+            y = self.ypadding
+         default:
+            exit(1)
+         }
+
+         self.setFrameOrigin(NSPoint(x: x, y: y))
+      }
+      //        self.setContentSize(NSSize(width: width, height: height))
+   }
+
+   public func getOrientation() -> Int {
+      return self.orientation
+   }
+
+   //    public func setStickToWindow(win: EvasiveWindow) {
+   //        self.stickToWindow = win
+   //        self.frame.size.width = self.stickToWindow.frame.size.width
+   //        self.refreshOrigin()
+   //    }
+
+   override func mouseEntered(with event: NSEvent) {
+      super.mouseEntered(with: event)
+      print("mouse entered")
+      self.move()
+   }
+
+   override func mouseExited(with event: NSEvent) {
+      super.mouseExited(with: event)
+      print("mouse exited")
+   }
+
+   override func mouseDown(with event: NSEvent) {
+      super.mouseDown(with: event)
+      print("mouse down")
+      //        self.move()
+   }
+
+   override func rightMouseDown(with event: NSEvent) {
+      super.rightMouseDown(with: event)
+      print("right mouse button down")
+      self.move()
+   }
 }
